869230e152f475c14eea5d8dbea5f00e2ab5e36c
dzikribdr <dzikriabdr@gmail.com>
2025-11-07 11:36:02 +0700

    Pembuatan final splash screen 1

--- lib/screen/splash1.dart
+++ lib/screen/splash1.dart
@@ -1,10 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:uts_dzikri/screen/splash2.dart';
 
 class MySplash1 extends StatelessWidget {
   const MySplash1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(height: 20),
+          Container(
+            margin: const EdgeInsets.only(bottom: 25),
+            width: 250,
+            height: 250,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white,
+              image: DecorationImage(
+                image: AssetImage("assets/ppl1.jpeg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 40.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 8),
+          const Text(
+            "Forgot to bring your wallet \n when you are shopping?",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18.0,
+            ),
+          ),
+          const SizedBox(height: 16),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 10,
+                height: 10,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red,
+                ),
+              ),
+              const SizedBox(width: 10),
+              Container(
+                width: 10,
+                height: 10,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(width: 10),
+              Container(
+                width: 10,
+                height: 10,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(width: 10),
+            ],
+          ),
+
+          const SizedBox(height: 20),
+          Container(
+            margin: const EdgeInsetsDirectional.only(start: 100.0, end: 100.0),
+            child: SizedBox(
+              height: 50,
+              width: 400,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MySplash2()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                ),
+                child: const Text(
+                  "Continue",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
   }
 }
