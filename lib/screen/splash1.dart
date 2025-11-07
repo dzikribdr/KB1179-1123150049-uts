865dfe9a62c690ec6e4e831afaa5cc595ac99644
dzikribdr <dzikriabdr@gmail.com>
2025-11-07 11:18:48 +0700

    Pembuatan file main, file splash1

---
+++ lib/screen/splash1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MySplash1 extends StatelessWidget {
+  const MySplash1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
